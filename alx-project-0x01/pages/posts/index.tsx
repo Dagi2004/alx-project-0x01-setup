187a6f5c133c0e9e95e95f8d2edb6505127c8e7b
Dagi <dagmawimilkias@gmail.com>
2024-12-22 03:26:39 +0300

    React Architect

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const Index: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
+      <Header />
       <h1>Welcome to our Application!"</h1>
     </div>
   );
