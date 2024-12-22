c95094709544e2db6584cd4ace369162d13aedbf
Dagi <dagmawimilkias@gmail.com>
2024-12-22 03:08:10 +0300

    React Architect

---
+++ alx-project-0x01/components/common/Button.tsx
@@ -0,0 +1,8 @@
+const Button: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button className="text-7xl font-thin">Button</button>
+    </div>
+  );
+};
+export default Home;
